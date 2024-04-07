3719934410b5d52ec52c97ce784838c02adcb877
Pranav <prasadpranav736@gamil.com>
2024-04-07 22:20:06 +0530

    snowpack build added

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,27 +1,33 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-	mount: {
-		public: { url: '/', static: true },
-		src: { url: '/dist' }
-	},
-	plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv', '@snowpack/plugin-babel','@snowpack/plugin-postcss'],
-	routes: [
-		/* Enable an SPA Fallback in development: */
-		{ match: 'routes', src: '.', dest: '/index.html' },
-        // for local devleopment and direct routing
-        // { match: 'routes', src: '.*', dest: '/index.html' }
-	],
-	optimize: {
-		/* Example: Bundle your final build: */
-		// "bundle": true,
-	},
-	packageOptions: {
-		/* ... */
-	},
-	devOptions: {
-		tailwindConfig: "./tailwind.config.js"
-	},
-	buildOptions: {
-		/* ... */
-	}
-}
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-babel',
+    '@snowpack/plugin-postcss',
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    { match: 'routes', src: '.', dest: '/index.html' },
+    // for local devleopment and direct routing
+    // { match: 'routes', src: '.*', dest: '/index.html' }
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    tailwindConfig: './tailwind.config.js',
+  },
+  buildOptions: {
+    /* ... */
+    out: 'build', // Specify the output directory here
+  },
+};
